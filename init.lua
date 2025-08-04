ed980ee512ed8bba02c6973e216a1c04c25c286c
Arvin Verain <arvinverain@proton.me>
2025-08-04 20:56:31 +0800

    chore: Merge lazy setup

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 require 'config.options'
 require 'config.keymaps'
-require 'config.lazy-bootstrap'
-require 'config.lazy-plugins'
 require 'config.autocmds'
+require 'config.lazy'
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,22 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require('lazy').setup({
+  require 'plugins.treesitter',
   require 'plugins.guess-indent',
   require 'plugins.gitsigns',
   require 'plugins.which-key',
@@ -9,7 +27,6 @@ require('lazy').setup({
   require 'plugins.tokyonight',
   require 'plugins.todo-comments',
   require 'plugins.mini',
-  require 'plugins.treesitter',
   require 'plugins.debug',
   require 'plugins.indent-blankline',
   require 'plugins.lint',
