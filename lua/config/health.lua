73d6363bf4d44a383fa3568a1dfad90c8900ba0e
Arvin Verain <arvinverain@proton.me>
2025-08-05 20:24:14 +0800

    chore: Update health file

--- lua/config/health.lua
+++ lua/config/health.lua
@@ -27,16 +27,9 @@ end
 
 return {
   check = function()
-    vim.health.start 'kickstart.nvim'
-
-    vim.health.info [[NOTE: Not every warning is a 'must-fix' in `:checkhealth`
-
-  Fix only warnings for plugins and languages you intend to use.
-    Mason will give warnings for languages that are not installed.
-    You do not need to install, unless you want to use those languages!]]
-
-    local uv = vim.uv or vim.loop
-    vim.health.info('System Information: ' .. vim.inspect(uv.os_uname()))
+    vim.health.start 'config'
+    vim.health.info [[Printing system information and checking external dependencies...]]
+    vim.health.info('System Information: ' .. vim.inspect(vim.uv.os_uname()))
 
     check_version()
     check_external_reqs()
