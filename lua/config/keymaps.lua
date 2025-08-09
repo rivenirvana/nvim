dc1e4a12e1cf6e644fc2cbc9892554e38e53370f
Arvin Verain <arvinverain@proton.me>
2025-08-09 20:40:34 +0800

    feat: Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,19 +14,21 @@ vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, { desc = '[F]ormat buffer v
 -- https://github.com/nvim-lua/kickstart.nvim/issues/1121
 -- vim.keymap.set('i', '<C-c>', '<C-c>')
 
--- Move visual selection/s to lines below or above them
-vim.keymap.set('x', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('x', 'K', ":m '<-2<CR>gv=gv")
+-- vim.keymap.set('n', 'J', ':m .+1<CR>==', { desc = 'Move line down' })
+-- vim.keymap.set('n', 'K', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('x', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('x', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set('x', '<', '<gv', { desc = 'Indent left (keep selection)' })
+vim.keymap.set('x', '>', '>gv', { desc = 'Indent right (keep selection)' })
 
--- Retain cursor postion for:
--- - joining lines
-vim.keymap.set('n', 'J', 'mzJ`z')
--- - half-page scrolls
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
--- - matching searches
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join line (retain cursor position)' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page down (centered)' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Half page up (centered)' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result (centered)' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result (centered)' })
+
+vim.keymap.set('n', 'zb', ':let save_so = &so<CR>:set so=0<CR>zb:let &so=save_so<CR>', { desc = 'Drag cursor line to bottom of window', silent = true })
+vim.keymap.set('n', 'zt', ':let save_so = &so<CR>:set so=0<CR>zt:let &so=save_so<CR>', { desc = 'Drag cursor line to top of window', silent = true })
 
 vim.keymap.set({ 'n', 'x' }, '<leader>y', [["+y]], { desc = 'Yank selection to system clipboard' })
 -- This keymap doesn't seem to work correctly as the register command that it's mapped to. Probably needs to be reported.
@@ -36,8 +38,5 @@ vim.keymap.set({ 'n', 'x' }, '<leader>p', [["+p]], { desc = 'Paste from system c
 vim.keymap.set({ 'n', 'x' }, '<leader>P', [["+P]], { desc = 'Paste from system clipboard' })
 vim.keymap.set({ 'n', 'x' }, '<leader>v', [["_d]], { desc = 'Delete to void' })
 vim.keymap.set('x', '<leader>V', [["_dP]], { desc = 'Paste over selection' })
-
-vim.keymap.set('n', 'zt', ':let save_scrolloff = &scrolloff<CR>:set scrolloff=0<CR>zt:let &scrolloff = save_scrolloff<CR>')
-vim.keymap.set('n', 'zb', ':let save_scrolloff = &scrolloff<CR>:set scrolloff=0<CR>zb:let &scrolloff = save_scrolloff<CR>')
 
 -- vim: ts=2 sts=2 sw=2 et
