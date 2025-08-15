7a24a6f41028d04877e8af7aa80eabe60bd353cc
Arvin Verain <arvinverain@proton.me>
2025-08-16 00:37:49 +0800

    fix(vim-suda): Don't auto enable and lazy load via cmd

--- lua/plugins/vim-suda.lua
+++ lua/plugins/vim-suda.lua
@@ -4,11 +4,7 @@ return {
   -- Workaround to sudo not working in-editor
   -- https://github.com/neovim/neovim/issues/1716
   'lambdalisue/vim-suda',
-  init = function()
-    if not vim.o.diff then
-      vim.g.suda_smart_edit = 1
-    end
-  end,
+  cmd = { 'SudaRead', 'SudaWrite' },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
