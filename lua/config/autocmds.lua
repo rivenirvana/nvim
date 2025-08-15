30207a1a8e9aea85622913c8965e9bbc38dd7937
Arvin Verain <arvinverain@proton.me>
2025-08-15 22:46:37 +0800

    feat: Fix kitty color sync autocmds
    - Remove M table
    - Restructure color data
    - Remove escape code builder function
    - Account for all colors dynamically
    - Add autocmd descriptions

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,12 +1,14 @@
-local M = {}
+local kitty_augroup = vim.api.nvim_create_augroup('KittyColorSync', { clear = true })
 
-M.augroup = vim.api.nvim_create_augroup('KittyColorSync', {})
-M.colors = {
-  background = 'Normal',
-  transparent_background_color1 = 'ColorColumn',
-  transparent_background_color2 = 'CursorLine',
-  transparent_background_color3 = 'StatusLine',
-  transparent_background_color4 = 'NormalFloat',
+local kitty_colors = {
+  { key = 'background', blend = false, hl = 'Normal' },
+  { key = 'transparent_background_color1', blend = false, hl = 'ColorColumn' },
+  { key = 'transparent_background_color2', blend = false, hl = 'CursorLine' },
+  { key = 'transparent_background_color3', blend = false, hl = 'StatusLine' },
+  { key = 'transparent_background_color4', blend = false, hl = 'NormalFloat' },
+  { key = 'transparent_background_color5', blend = true, hl = 'Normal' },
+  { key = 'transparent_background_color6', blend = true, hl = 'NormalNC' },
+  { key = 'transparent_background_color7', blend = true, hl = 'CursorLine' },
 }
 
 local function rgb_blend(ratio, under, over)
@@ -27,70 +29,68 @@ local function rgb_blend(ratio, under, over)
   return bit.lshift(mr, 16) + bit.lshift(mg, 8) + mb
 end
 
-local function send_color_code(params)
-  local code = { '\x1b]21' }
-  for _, param in ipairs(params) do
-    table.insert(code, param)
-  end
-  table.insert(code, '\x1b\\')
-  io.stdout:write(table.concat(code, ''))
-end
-
 vim.api.nvim_create_autocmd({ 'UIEnter', 'BufWinEnter', 'ColorScheme', 'FileType', 'WinEnter' }, {
-  group = M.augroup,
+  group = kitty_augroup,
   callback = function()
-    local params = {}
-    for key, grp in pairs(M.colors) do
-      local hl = vim.api.nvim_get_hl(0, { name = grp })
-      if hl and hl.bg then
-        table.insert(params, ';')
-        table.insert(params, key)
-        table.insert(params, '=')
-        table.insert(params, string.format('#%06x', hl.bg))
+    local code = { '\x1b]21' }
+
+    for _, color in ipairs(kitty_colors) do
+      if not color.blend and color.hl ~= '' then
+        local hl = vim.api.nvim_get_hl(0, { name = color.hl })
+        if hl and hl.bg then
+          table.insert(code, ';')
+          table.insert(code, color.key)
+          table.insert(code, '=')
+          table.insert(code, string.format('#%06x', hl.bg))
+        end
       end
     end
 
-    send_color_code(params)
+    table.insert(code, '\x1b\\')
+    io.stdout:write(table.concat(code))
   end,
 })
 
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-  group = M.augroup,
+  group = kitty_augroup,
   pattern = { '*_backdrop' },
   callback = function(args)
     local id = vim.fn.win_findbuf(args.buf)[1]
     local ratio = vim.wo[id].winblend
-    -- local ratio = vim.api.nvim_get_option_value('winblend', { win = id })
-    local params = {}
-    local hl = vim.api.nvim_get_hl(0, { name = 'Normal' })
-    if hl and hl.bg then
-      local blend = rgb_blend(ratio, hl.bg, 0)
-      table.insert(params, ';transparent_background_color5=')
-      table.insert(params, string.format('#%06x', blend))
-    end
-    local hl2 = vim.api.nvim_get_hl(0, { name = 'NormalNC' })
-    if hl and hl2.bg then
-      local blend2 = rgb_blend(ratio, hl2.bg, 0)
-      table.insert(params, ';transparent_background_color6=')
-      table.insert(params, string.format('#%06x', blend2))
+    local code = { '\x1b]21' }
+
+    for _, color in ipairs(kitty_colors) do
+      if color.blend and color.hl ~= '' then
+        local hl = vim.api.nvim_get_hl(0, { name = color.hl })
+        if hl and hl.bg then
+          local blend = rgb_blend(ratio, hl.bg, 0)
+          table.insert(code, ';')
+          table.insert(code, color.key)
+          table.insert(code, '=')
+          table.insert(code, string.format('#%06x', blend))
+        end
+      end
     end
 
-    send_color_code(params)
+    table.insert(code, '\x1b\\')
+    io.stdout:write(table.concat(code))
   end,
 })
 
 vim.api.nvim_create_autocmd({ 'UILeave' }, {
-  group = M.augroup,
+  group = kitty_augroup,
   callback = function()
-    local params = {}
-    for key in pairs(M.colors) do
-      table.insert(params, ';')
-      table.insert(params, key)
-    end
-    table.insert(params, ';transparent_background_color5')
-    table.insert(params, ';transparent_background_color6')
+    local code = { '\x1b]21' }
 
-    send_color_code(params)
+    for _, color in ipairs(kitty_colors) do
+      if color.hl ~= '' then
+        table.insert(code, ';')
+        table.insert(code, color.key)
+      end
+    end
+
+    table.insert(code, '\x1b\\')
+    io.stdout:write(table.concat(code))
   end,
 })
 
