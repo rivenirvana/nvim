c44b0b9fc5274dc9d295d9eaaa3dc37f7aac7295
Arvin Verain <arvinverain@proton.me>
2025-08-18 22:10:10 +0800

    refactor(oil): Do not use lazy's keys spec

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,28 +2,27 @@
 ---@type LazySpec
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    keymaps = {
-      ['<Esc>'] = { 'actions.close', mode = 'n' },
-      ['q'] = { 'actions.close', mode = 'n' },
-    },
-    view_options = {
-      show_hidden = true,
-    },
-  },
-  dependencies = {
-    {
-      'nvim-tree/nvim-web-devicons',
-      -- 'echasnovski/mini.icons',
-      opts = {},
-    },
-  },
   lazy = false,
-  keys = {
-    { '-', '<CMD>Oil --float<CR>', desc = 'Oil: Open parent directory' },
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons', opts = {} }, -- 'echasnovski/mini.icons',
   },
+  -- ---@module 'oil'
+  -- ---@type oil.SetupOpts
+  -- opts = {},
+  config = function()
+    local oil = require 'oil'
+    oil.setup {
+      keymaps = {
+        ['<Esc>'] = { 'actions.close', mode = 'n' },
+        ['q'] = { 'actions.close', mode = 'n' },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set('n', '-', oil.open_float, { desc = 'Oil: Open Parent Directory' })
+  end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
