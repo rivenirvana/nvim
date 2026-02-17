b779e06bd67423b72d9881cc7fa06713c17f73b5
Arvin Verain <arvinverain@proton.me>
2026-02-17 11:19:54 +0800

    feat: Update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,40 +6,9 @@ return {
   ---@module 'which-key'
   ---@type wk.Opts
   opts = {
-    delay = 200,
-    icons = {
-      mappings = vim.g.has_nerd_font,
-      keys = vim.g.has_nerd_font and {} or {
-        Up = '<Up> ',
-        Down = '<Down> ',
-        Left = '<Left> ',
-        Right = '<Right> ',
-        C = '<C-…> ',
-        M = '<M-…> ',
-        D = '<D-…> ',
-        S = '<S-…> ',
-        CR = '<CR> ',
-        Esc = '<Esc> ',
-        ScrollWheelDown = '<ScrollWheelDown> ',
-        ScrollWheelUp = '<ScrollWheelUp> ',
-        NL = '<NL> ',
-        BS = '<BS> ',
-        Space = '<Space> ',
-        Tab = '<Tab> ',
-        F1 = '<F1>',
-        F2 = '<F2>',
-        F3 = '<F3>',
-        F4 = '<F4>',
-        F5 = '<F5>',
-        F6 = '<F6>',
-        F7 = '<F7>',
-        F8 = '<F8>',
-        F9 = '<F9>',
-        F10 = '<F10>',
-        F11 = '<F11>',
-        F12 = '<F12>',
-      },
-    },
+    preset = 'helix',
+    delay = 0,
+    icons = { mappings = vim.g.has_nerd_font },
 
     spec = {
       { '<leader>s', group = 'Search', mode = { 'n', 'v' } },
@@ -48,7 +17,7 @@ return {
       { '<leader>t', group = 'Toggle' },
       { '<leader>x', group = 'Exec' },
       { '<leader>c', group = 'Conform' },
-      { 'gr', group = 'LSP', mode = { 'n' } },
+      { 'gr', group = 'LSP' },
     },
 
     -- Disable which-key popup when entering visual mode
