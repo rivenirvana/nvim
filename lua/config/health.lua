f2580e19910ed61ec859c48199b42dd79d0706cd
Arvin Verain <arvinverain@proton.me>
2026-02-17 11:17:37 +0800

    refactor: Simplify healthcheck

--- lua/config/health.lua
+++ lua/config/health.lua
@@ -1,19 +1,28 @@
 local function check_version()
   local ver = tostring(vim.version())
-  if vim.fn.has 'nvim-0.11.2' == 1 then
-    vim.health.ok("Neovim version is: '" .. ver .. "'")
+  if vim.fn.has 'nvim-0.12.0' == 1 then
+    vim.health.ok('Neovim version: ' .. ver)
   else
-    vim.health.error("Neovim out of date: '" .. ver .. "'. Upgrade to latest stable or nightly")
+    vim.health.error('Neovim outdated: ' .. ver)
   end
 end
 
 local function check_external_reqs()
-  for _, exe in ipairs { 'git', 'make', 'unzip', 'rg', 'fzf' } do
+  for _, exe in ipairs {
+    'git',
+    'make',
+    'unzip',
+    'rg',
+    'fd',
+    'fzf',
+    'tree-sitter',
+    'node',
+  } do
     local is_executable = vim.fn.executable(exe) == 1
     if is_executable then
-      vim.health.ok("Found executable: '" .. exe .. "'")
+      vim.health.ok('Found: ' .. exe)
     else
-      vim.health.warn("Could not find executable: '" .. exe .. "'")
+      vim.health.warn('Could not find: ' .. exe)
     end
   end
 end
